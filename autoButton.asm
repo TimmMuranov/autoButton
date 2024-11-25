1d079b88459245d9b0e881985c0ca0535bde8f78
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-25 23:53:45 +0300

    поф на ассемблер

--- autoButton.asm
+++ autoButton.asm
@@ -1,12 +1,8 @@
-;--- Регистры портов ---
-.equ DDRB, 0x17
-.equ PORTB, 0x18
-;-----------------------
-.equ PB0, 0x00
-.equ PB1, 0x01
-.equ PB2, 0x02
-;---- 0-я координата ---
-.ORG 0x0000
-;--- Настройка портов --
-sbi DDRB, PB0
-;-----------------------
+#include <iostream>
+#include <arduino.h>
+
+using namespace std;
+
+int main(){
+  
+}
