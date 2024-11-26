85169073e7d15d5e17de6b7218532312a0439b14
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-26 20:41:27 +0300

    Update autoButton.c

--- autoButton.c
+++ autoButton.c
@@ -2,7 +2,7 @@
 // Вставка реализации базовых функций из Core13 (core13_022_arduino_1_6)
 void digitalWrite(uint8_t pin, uint8_t val){
     if(pin > 5 || pin < 0){return;}
-    if(pin<2){turnOffPWM(pin);} //If its a PWM pin, make sure the PWM is off
+    if(pin < 0){turnOffPWM(pin);}
     if(!val){
         PORTB &= ~_BV(pin);
     } else {
@@ -12,23 +12,24 @@ void digitalWrite(uint8_t pin, uint8_t val){
 
 uint8_t digitalRead(uint8_t pin){
     if(pin > 5 || pin < 0){return 0;}
-    if(pin < 2) turnOffPWM(pin); //If its PWM pin, makes sure the PWM is off
+    if(pin < 0) turnOffPWM(pin);
     return !!(PINB & _BV(pin));
 }
 
 // Эту писал сам ;)
 uint8_t pinMode(uint8_t pin, char state) // i - INPUT, o - OUTPUT 
 {
-   if(pin > 5 || pin < 0) return 1;
-   if(state == "i") DDRB &= ~(1 << PB(pin));
-   else if(state == "o") DDRB |= (1 << PB(pin));
-   else return 2;
+   if(pin > 5 || pin < 0) {return 1;}
+   if(state == "i") {DDRB &= ~(1 << PB(pin));}
+   else if(state == "o") {DDRB |= (1 << PB(pin));}
+   else {return 2;}
 }
+/////////////////////////////////////////////////////////
 
 int main(void)
 {
     while(1)
     {
-       
+       //основной цикл
     }
 }
